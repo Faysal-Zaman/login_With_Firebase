6b29419e3eda9a2d74aaeec21ace27fefb3e9705
Faysal-Zaman <faysalzuman00@gmail.com>
2022-09-04 13:55:53 +0500

    second commit

--- lib/screens/upload_page.dart
+++ lib/screens/upload_page.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -16,6 +17,7 @@ class UploadPage extends StatefulWidget {
 class _UploadPageState extends State<UploadPage> {
   PlatformFile? pickedFile;
   UploadTask? uploadTask;
+  bool cond = false;
 
   Future uploadFile() async {
     final path = 'files/${pickedFile!.name}';
@@ -32,6 +34,7 @@ class _UploadPageState extends State<UploadPage> {
 
     setState(() {
       uploadTask = null;
+      cond = true;
     });
   }
 
@@ -46,52 +49,100 @@ class _UploadPageState extends State<UploadPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "Upload Your File",
-          style: TextStyle(color: Colors.white),
+    return SafeArea(
+      bottom: false,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "Upload Files to Firebase",
+            style: TextStyle(color: Colors.amber),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
         ),
-        elevation: 10,
-        backgroundColor: Colors.amber,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (pickedFile != null)
-              Expanded(
-                child: Center(
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 20, right: 20),
-                    child: Image.file(
-                      File(pickedFile!.path!),
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              (pickedFile != null)
+                  ? Expanded(
+                      child: Center(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 20, right: 20),
+                          child: Image.file(
+                            File(pickedFile!.path!),
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    )
+                  : SizedBox(
+                      height: 200,
+                      child:
+                          Image.asset('assets/butterfly.png', fit: BoxFit.fill),
                     ),
-                  ),
-                ),
+              const SizedBox(
+                height: 50,
               ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: selectFile,
-              child: const Text("Select File"),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: uploadFile,
-              child: const Text("Upload File"),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            BuildProgress(uploadTask: uploadTask),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: selectFile,
+                child: const Text("Select File"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: uploadFile,
+                child: const Text("Upload File"),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              BuildProgress(uploadTask: uploadTask),
+              (cond == true)
+                  ? SizedBox(
+                      width: double.infinity,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: ((context) =>
+                                        const UploadPage())));
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          alignment: Alignment.center,
+                          backgroundColor: Colors.amber,
+                          elevation: 0,
+                          textStyle: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                        child: const Text(
+                          "Click to upload another file",
+                          style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    )
+                  : const SizedBox(
+                      height: 0,
+                      width: 0,
+                    )
+            ],
+          ),
         ),
       ),
     );
